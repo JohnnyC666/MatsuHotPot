5b7abc95189160c672c8a2071d540403388d0bed
JohnnyC666 <129473277+JohnnyC666@users.noreply.github.com>
2023-04-03 15:56:20 +0800

    modify

--- MatsuHotPot/ViewController.swift
+++ MatsuHotPot/ViewController.swift
@@ -25,41 +25,36 @@ class ViewController: UIViewController {
     
     @IBAction func logIn(_ sender: UIButton) {
         let phone = self.phoneNumber.text
-        let password = self.Password.text
+        func isPhoneNo(_ phoneNumber : String) -> Bool{
+            let phoneRegex = "^09\\d{8}$"
+            let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)//？？？？？？？？？？？？？
+            return phoneTest.evaluate(with: phoneNumber)
+        }
         
-    
-        if phone == "12345678" && password == "111"{
-            
+        let password = self.Password.text
+        func isCorrectPassword(_ passWord : String) -> Bool{
+            let passwordRegex = "^\\d{6}$"
+            let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+            return passwordTest.evaluate(with: passWord)
+        }
+        
+        if isPhoneNo(phone!) == true && isCorrectPassword(password!){
         }else{
-            let alert = UIAlertController(title: "登入失敗", message: "帳號或密碼錯誤", preferredStyle: .alert)
+            if let soundURL = Bundle.main.url(forResource: "incorrect", withExtension: "mp3"){
+                var soundID: SystemSoundID = 0
+                AudioServicesCreateSystemSoundID(soundURL as CFURL, &soundID)
+                AudioServicesPlayAlertSound(soundID)
+            }
+            let alert = UIAlertController(title: "登入失敗", message: "電話號碼或密碼錯誤", preferredStyle: .alert)
             alert.addAction(
-                UIAlertAction(
-                    title: "確定",
-                    style: .default,
-                    handler:
-                        {
-                          (a)->Void in
-                            guard let soundURL = Bundle.main.url(forResource: "test", withExtension: "mp3") else {
-                                    print("Could not find sound file.")
-                                    return
-                                }
-                                do {
-                                    let player = try AVAudioPlayer(contentsOf: soundURL)
-                                    player.prepareToPlay()
-                                    player.play()
-                                } catch {
-                                    print("Could not play sound: \(error.localizedDescription)")
-                                }
-                        }
-                )
+                UIAlertAction(title: "確定", style: .default, handler: nil)
             )
             present(alert, animated: true, completion: nil)
         }
     }
-    
+     
     @IBAction func register(_ sender: UIButton) {
+        
     }
-    
-    
 }
 
